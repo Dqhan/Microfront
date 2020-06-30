fae44ca70167ecd19ec31ba60c2e654e98b937b4
Dqhan <291927006@qq.com>
2020-06-30 17:37:13 +0800

    test

--- config/single-spa-config.js
+++ config/single-spa-config.js
@@ -1,30 +1,84 @@
 // single-spa-config.js
 import * as singleSpa from "single-spa";
 
+// var config = [
+//   // {
+//   //   name: "base",
+//   //   path: "",
+//   //   index: "/base/index.js",
+//   //   store: "/base/store.js",
+//   //   base: true,
+//   // },
+//   {
+//     name: "common",
+//     path: "/",
+//     index: "/src/app-common/index.js",
+//     store: "/src/app-common/store.js",
+//     base: false,
+//   },
+//   {
+//     name: "app-react",
+//     path: "/app-react",
+//     index: "/src/app-react/index.js",
+//     store: "/src/app-react/store.js",
+//     base: false,
+//   },
+//   {
+//     name: "app2-react",
+//     path: "/app2-react",
+//     index: "/src/app2-react/index.js",
+//     store: "/src/app2-react/store.js",
+//     base: false,
+//   },
+// ];
+
+// function register() {
+//   config.forEach((c) => {
+//     singleSpa.registerApplication(
+//       c.name,
+//       () => {
+//         var path = c.index;
+//         return System.import(path);
+//       },
+//       () => {
+//         return c.base === true ? true : location.pathname === c.path;
+//       }
+//     );
+//   });
+// }
+
+// register();
+
 // 注册应用
 singleSpa.registerApplication(
-  "/common",
+  "base",
+  () => System.import("../src/base/index.js"),
   () => {
-    return import("../src/app-common/index.js")
-  },
+    return true;
+  }
+);
+
+singleSpa.registerApplication(
+  "common",
+  () => System.import("../src/app-common/index.js"),
   () => {
-    return location.pathname.startsWith("/");
+    return location.pathname === "/";
   }
 );
 
 singleSpa.registerApplication(
   "react",
-  () => import("../src/app-react/index.js"),
+  () => System.import("../src/app-react/index.js"),
   (location) => {
-    return location.pathname.startsWith("/react-1")
+    return location.pathname.startsWith("/react-1");
   }
 );
 
 singleSpa.registerApplication(
   "react2",
-  () => import("../src/app2-react/index.js"),
+  () => System.import("../src/app2-react/index.js"),
   () => {
-    return location.pathname.startsWith("/react-2") 
+    return location.pathname.startsWith("/react-2");
   }
 );
 
--- src/app-common/root.component.js
+++ src/app-common/root.component.js
@@ -1,16 +1,28 @@
-import { Route, Link, Switch, BrowserRouter, HashRouter } from "react-router-dom";
-
+import {
+  Route,
+  Link,
+  Switch,
+  BrowserRouter,
+  HashRouter,
+} from "react-router-dom";
 
 function Test() {
-  return <div>test</div>
+  return <div>test</div>;
 }
 
 function App() {
   return (
     <div>
       <h1>common app</h1>
-      <BrowserRouter>
-        {/* <Route path="/test" component={Test} ></Route> */}
+      <ul>
+        <li>
+          <a href="/react-1">react app1</a>
+        </li>
+        <li>
+          <a href="/react-2">react app2</a>
+        </li>
+      </ul>
+      {/* <BrowserRouter>
         <ul>
           <li>
             <Link to="react-1"> React app</Link>
@@ -18,11 +30,8 @@ function App() {
           <li>
             <Link to="/react-2"> React app2</Link>
           </li>
-          {/* <li>
-            <Link to="/test"> test</Link>
-          </li> */}
         </ul>
-      </BrowserRouter>
+      </BrowserRouter> */}
     </div>
   );
 }
